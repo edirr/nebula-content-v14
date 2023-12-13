c788575e2a62aa33d77b6c8659ce84e7423157ae
edirr <edirr92@aol.com>
2023-12-13 15:40:10 -0500

    added hw

---
+++ Module 1 - Introduction to Software Engineering/Lesson-3-JavaScript Basics/practice/Numbers-Practice-2.js
@@ -0,0 +1,31 @@
+
+// For these questions use the Math methods that JavaScript includes
+// All Math methods: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math
+
+
+// find the square root of 25
+
+
+// find the value of 3 to the power of 5
+
+
+// find the value of 7.4 rounded
+
+
+// find the rounded-up value of 7.4
+
+
+
+// find the absolute value of -13
+
+
+// find the minimum value in a set of numbers: 12,13,14,15,2,-1
+
+
+// find the maximum value in a set of numbers: 12,13,14,15,2,-1
+
+
+// create a random number 
+
+
+// Create a random number between 0-10
